45bfa358eaad6770236a5173ee9531d000d90d60
PPetrov <syncredex@gmail.com>
2023-06-03 20:15:22 +0300

    Create: create util classes for the data layer

--- lib/util/service/service_locator.dart
+++ lib/util/service/service_locator.dart
@@ -1,5 +1,6 @@
 import 'package:get_it/get_it.dart';
 
+import '../helper/logger_helper.dart';
 import 'retrofit_service.dart';
 
 GetIt locator = GetIt.instance;
@@ -7,4 +8,5 @@ GetIt locator = GetIt.instance;
 Future<void> setupLocator() async {
   // Register services
   locator.registerLazySingleton<RetrofitService>(() => RetrofitService());
+  locator.registerFactory<LoggerHelper>(() => LoggerHelper());
 }
