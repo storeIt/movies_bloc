904c587a0897aca1341776f0d265796f288ed092
PPetrov <syncredex@gmail.com>
2023-06-04 00:29:09 +0300

    Create: guarded zones to log unhandled exceptions

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,23 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'feature/app/view/app.dart';
+import 'util/helper/logger_helper.dart';
 import 'util/service/service_locator.dart';
 
 void main() {
   setupLocator();
+
+  FlutterError.onError = (FlutterErrorDetails details) {
+    WidgetsFlutterBinding.ensureInitialized();
+    if (kDebugMode) locator<LoggerHelper>().e('FlutterError', details.exception, details.stack);
+  };
+
+  runZonedGuarded(() {
+    runApp(const App());
+  }, (error, stack) {
+    if (kDebugMode) locator<LoggerHelper>().e('$error', error, stack);
+  });
 }
